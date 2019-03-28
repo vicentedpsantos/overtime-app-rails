52071f52ac6492a2dd592cb8c32e39c49b3b15ae
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-27 23:07:15 -0300

    Implemented creation process for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
   end
 
@@ -8,17 +10,24 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   private
 
   def post_params
     params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,29 +2,27 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
+    before { visit posts_path }
     it 'can be reached succesfully' do
-      visit posts_path
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
 
   describe 'creation' do
+    before { visit new_post_path}
+
     it 'has a new form that can be reached' do
-      visit new_post_path
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
-      visit new_post_path
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-      
+
       click_on 'Save'
 
       expect(page).to have_content('Some rationale')
