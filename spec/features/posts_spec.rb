591729cee5ebe5fac7ee38779a956ae34ef05a14
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-28 01:08:55 -0300

    Listed all time entries

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'John', last_name: 'Snow')
+    login_as(user, scope: :user)
+  end
+
   describe 'index' do
-    before { visit posts_path }
+    before do
+      visit posts_path
+    end
+
     it 'can be reached succesfully' do
       expect(page.status_code).to eq(200)
     end
@@ -10,14 +18,17 @@ describe 'navigate' do
     it 'has a title of Posts' do
       expect(page).to have_content(/Posts/)
     end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: 'Post1')
+      post2 = Post.create(date: Date.tomorrow, rationale: 'Post2')
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
   end
 
   describe 'creation' do
-    before do
-      user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'John', last_name: 'Snow')
-      login_as(user, scope: :user)
-      visit new_post_path
-    end
+    before { visit new_post_path }
 
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
