112aedbfdf3b757dd90d82a7868dd29fac7d13ea
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-05 20:53:22 -0300

    Implemented initial view code for audit logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :audit_logs
-  namespace :admin do
-      resources :users
-      resources :posts
-      resources :admin_users
+  resources :audit_logs, except: %i[new edit destroy]
 
-      root to: "users#index"
-    end
+  namespace :admin do
+    resources :users
+    resources :posts
+    resources :admin_users
+
+    root to: 'users#index'
+  end
   resources :posts
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
