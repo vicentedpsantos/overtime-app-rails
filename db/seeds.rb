8b6b4967e98ec6ee8d441ab6307a13f470c637c2
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-10 20:37:11 -0300

    Built out rake task to send sms messages on fridays to notify employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create(email: 'test@test.com',
+@employee = Employee.create(email: 'test@test.com',
                     password: 'asdfasdf',
                     password_confirmation: 'asdfasdf',
                     first_name: 'John',
@@ -16,14 +16,14 @@ AdminUser.create(email: 'admin@test.com',
 
 puts '1 admin user created'
 
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts '3 Audit Logs have been created'
 
 10.times do |post|
-  Post.create!(date: Date.today, status: 0, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
-  Post.create!(date: Date.today, status: 1, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 4.5)
-  Post.create!(date: Date.today, status: 2, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 6.5)
+  Post.create!(date: Date.today, status: 0, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, status: 1, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 4.5)
+  Post.create!(date: Date.today, status: 2, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 6.5)
 end
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,15 +1,14 @@
 namespace :notification do
   desc 'Sends SMS notification to employees asking them to log if they had overtime or not'
   task sms: :environment do
-    # if Time.now.sunday?
-    #   # 1. Schedule to run at Sunday at 5pm
-    #   # 2. Iterate over all employees
-    #   # 3. Skip AdminUsers
-    #   # 4. Send a message that has instructions and a link to log time
-    #   # User.all.each do |user|
-    #   #   SmsTool.send_sms()
-    #   # end
-    # end
+    if Time.now.friday?
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard to request overtime or confirm your hours for last week: https://overtime-vdps-tracker.herokuapp.com/"
+
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
+    end
   end
 
   desc 'Sends email notification to managers (admin users) each day to inform of pending overtime requests'
