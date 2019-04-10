54b4a2cd8ac749d1c05e50f68b68e4d56251af22
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-09 22:17:33 -0300

    Finished styling homepage for employees

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -8,6 +8,7 @@ class AuditLogsController < ApplicationController
     audit_log = AuditLog.find(params[:id])
     authorize audit_log
     audit_log.confirmed!
+    audit_log.update(end_date: Date.today)
     redirect_to root_path
     flash[:success] = 'Thank you. Your confirmation has been successfully made!'
   end
